eb0942278877307daedcf5fac48588cd1e636e5a
AMIR <amirmsdi2412@gmail.com>
2026-08-29 13:03:02 +0330

    chore: make logo generic

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,8 +1,8 @@
 export const Logo = () => {
   return (
     <div className="flex items-center gap-1">
-      <span className="font-normal opacity-60">توسعه دهندگان</span>
-      <span className="font-bold text-primary">پذیرش۲۴</span>
+      <span className="font-normal opacity-60">راهنمای</span>
+      <span className="font-bold text-primary">نرم‌افزار</span>
     </div>
   );
 };
